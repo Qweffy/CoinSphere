a34e6d63f78cd8e6b777f77e444a08c1fbf38908
Qweffy <qweffooo@gmail.com>
2024-11-19 13:10:00 -0300

    feat: integrate Sentry for error tracking in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  enableNative: true,
+  debug: __DEV__,
+});
+
+function RootLayoutContent() {
   return <Stack />;
 }
+
+export default Sentry.wrap(RootLayoutContent);
